217fe76d56b91c4db35b5879684656f538868214
nataliaanto <nataantonoglou@gmail.com>
2024-10-22 20:23:50 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -8,11 +8,30 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     la a0, array
     li a1, 7    # unsigned
     li a2, 1
+
 prog:
-#-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
-#-----------------------------
+
+    li s0, 0    #arxikopoihsh tou s0=0 
+    beqz a1, done   
+
+    li t0, 4   #kathe leksi exei megethos 4 bytes            
+
+search_loop:
+
+    lw t1, 0(a0)              #fortwse th leksi apo th dieythinsi a0 ston t1
+    beq t1, a2, match_found   #an tostoixeio ston t1 tairiazei me ton  a2 pigaine sthn match_found
+    add a0, a0, t0            #prosthetoume 4 gia na metaboume sthn epomenh leksi 
+    addi a1, a1, -1           #meiwse to plithos twn stoixeiwn kata 1
+    bnez a1, search_loop      #an yparxoyn akoma stoixeia epanelave ton vrogxo 
+    j done                    #telos anazitisis pigaine ston done
+
+match_found:
+
+    mv s0, a0                 #apothikeysh ths dieuthinsis ston a0
+    add a0, a0, t0            #prosthetoume 4 gia na metavoume sthn epomenh leksi
+    addi a1, a1, -1           #meiwse to plithos kata 1
+    bnez a1, search_loop      #an yparxoun akoma stoixeia epanelave ton vrogxo
+
 done:
-    addi a7, zero, 10 
+    addi a7, zero, 10  #telos programmtos
     ecall
